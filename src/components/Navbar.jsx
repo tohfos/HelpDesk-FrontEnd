03c59304189fdf026c6f9a474683c3ed988768a6
EyadTamer999 <eyadhassan2002@gmail.com>
2023-11-27 00:12:37 +0200

    theme change added 😊

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useState } from 'react'
 import logo from '../logo.svg'
 
 const Navbar = () => {
+
+    const [theme, setTheme] = useState(localStorage.getItem('theme'))
+    const isChecked = theme === 'lofi' ? false : true
+
+    function toggleMode() {
+        console.log(theme)
+        if (theme === 'lofi') {
+            localStorage.setItem('theme', 'black')
+            setTheme('black')
+        } else {
+            localStorage.setItem('theme', 'lofi')
+            setTheme('lofi')
+        }
+    }
+
+    useEffect(() => {
+        //set data-theme attribute on html tag
+        document.documentElement.setAttribute('data-theme', localStorage.getItem('theme'))
+    }, [theme])
+
     return (
         <>
             <nav class="fixed w-full z-20 top-0 start-0 border-b shadow-lg">
@@ -19,12 +40,12 @@ const Navbar = () => {
                                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15" />
                                 </svg>
                             </button>
-                            <ul class="menu menu-lg dropdown-content mt-3 z-[1] p-2 shadow bg-accent rounded-box sticky">
-                                <li className='text-primary-content'><a>Homepage</a></li>
-                                <li className='text-primary-content'><a>Portfolio</a></li>
-                                <li className='text-primary-content'><a>About</a></li>
+                            <ul class="menu menu-lg dropdown-content mt-3 z-[1] p-2 shadow rounded-box sticky">
+                                <li><a>Homepage</a></li>
+                                <li><a>Portfolio</a></li>
+                                <li><a>About</a></li>
                                 <li> <label className="cursor-pointer grid place-items-center">
-                                    <input type="checkbox" value="black" className="toggle theme-controller bg-base-content row-start-1 col-start-1 col-span-2" />
+                                    <input type="checkbox" onClick={toggleMode} checked={isChecked} value={theme} className="toggle theme-controller bg-base-content row-start-1 col-start-1 col-span-2" />
                                     <svg className="col-start-1 row-start-1 stroke-base-100 fill-base-100" xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><circle cx="12" cy="12" r="5" /><path d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4" /></svg>
                                     <svg className="col-start-2 row-start-1 stroke-base-100 fill-base-100" xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg>
                                 </label>
@@ -36,7 +57,7 @@ const Navbar = () => {
 
                         <div class="items-center justify-between hidden w-full md:flex md:w-auto">
                             <label className="cursor-pointer grid place-items-center">
-                                <input type="checkbox" value="black" className="toggle theme-controller bg-base-content row-start-1 col-start-1 col-span-2" />
+                                <input type="checkbox" onClick={toggleMode} checked={isChecked} value={theme} className="toggle theme-controller bg-base-content row-start-1 col-start-1 col-span-2" />
                                 <svg className="col-start-1 row-start-1 stroke-base-100 fill-base-100" xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><circle cx="12" cy="12" r="5" /><path d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4" /></svg>
                                 <svg className="col-start-2 row-start-1 stroke-base-100 fill-base-100" xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg>
                             </label>
