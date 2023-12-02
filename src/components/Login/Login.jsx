39bb1ce5f298727ff16396640a3a1da6d488a5aa
EyadTamer999 <eyadhassan2002@gmail.com>
2023-12-02 14:30:16 +0200

    login works, kinda, fixed backend url issues

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,105 +1,94 @@
-import React, { Component } from 'react';
-import { useState } from 'react';
-export default class Login extends Component {
+import React from 'react'
+const Login = () => {
 
-    //create state variables for each input
-    constructor(props) {
-        super(props);
-        this.state = {
-            username: "",
-            password: ""
-        };
+    const [username, setUsername] = React.useState('')
+    const [password, setPassword] = React.useState('')
 
-        //bind handleChange function so we can use it to update state for each input
-        this.handleChange = this.handleChange.bind(this);
+    const handleUsernameChange = (e) => {
+        setUsername(e.target.value)
+        console.log(username)
     }
 
-    //update state based on form input changes
-    handleChange = (event) => {
-        this.setState({
-            [event.target.name]: event.target.value
-        });
-    };
+    const handlePasswordChange = (e) => {
+        setPassword(e.target.value)
+        console.log(password)
+    }
 
+    const handleLogin = async (e) => {
+        e.preventDefault()
 
-    render() {
+        console.log('login button clicked')
+        console.log(process.env.REACT_APP_EXPRESS_URL)
+        let input = { username, password }
 
-        //Log in user API call
-        const handleLogin = async () => {
-
-            console.log(this.state.username);
-            console.log(this.state.password);
-
-            // if (this.state.username === "" || this.state.password === "") {
-            //     return (<>
-            //         <div className="alert alert-error">
-            //             <div className="flex-1">
-            //                 <label className="mx-4">Please enter a username and password.</label>
-            //             </div>
-            //         </div>
-            //     </>)
-            // }
-
-            await fetch(`${process.env.BACKEND_URL}/auth/`, {
+        try {
+            const response = await fetch(`${process.env.REACT_APP_EXPRESS_URL}/auth/`, {
                 method: 'POST',
-                body: JSON.stringify({
-                    user: {
-                        username: this.state.username,
-                        password: this.state.password
-                    }
-                }),
-                headers: new Headers({
+                headers: {
                     'Content-Type': 'application/json'
-                })
+                },
+                body: JSON.stringify(input),
+                credentials: 'include'
             })
+            const data = await response.json()
+            console.log(data)
+            localStorage.setItem('token', data.token)
+
+        } catch (error) {
+            console.log(error)
         }
+    }
 
-
-        return (
-            <>
-                <div className="hero min-h-screen bg-base-200">
-                    <div className="hero-content flex-col lg:flex-row-reverse">
-                        <div className="text-center lg:text-left">
-                            <h1 className="text-5xl font-bold">Login now!</h1>
-                            <p className="py-6">Please log in to your account. Enter your <span className='font-semibold'>username</span> and <span className='font-semibold'>password</span> into the give fields.</p>
-                        </div>
-                        <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-                            <form className="card-body">
-                                <div className="form-control">
-                                    <label className="label">
-                                        <span className="label-text">Username</span>
-                                    </label>
-                                    <input
-                                        type="username"
-                                        placeholder="Enter your Username"
-                                        className="input input-bordered"
-                                        required
-                                    />
-                                </div>
-                                <div className="form-control">
-                                    <label className="label">
-                                        <span className="label-text">Password</span>
-                                    </label>
-                                    <input
-                                        type="password"
-                                        placeholder="Enter your password"
-                                        className="input input-bordered"
-                                        required
-                                    />
-                                    <label className="label">
-                                        <a href="#" className="label-text-alt link link-hover">
-                                            Forgot password?
-                                        </a>
-                                    </label>
-                                </div>
-                                <div className="form-control mt-6">
-                                    <button onClick={handleLogin} className="btn btn-primary">Login</button>
-                                </div>
-                            </form>
-                        </div>
+    return (
+        <>
+            <div className="hero min-h-screen bg-base-200">
+                <div className="hero-content flex-col lg:flex-row-reverse">
+                    <div className="text-center lg:text-left">
+                        <h1 className="text-5xl font-bold">Login now!</h1>
+                        <p className="py-6">Please log in to your account. Enter your <span className='font-semibold'>username</span> and <span className='font-semibold'>password</span> into the give fields.</p>
+                    </div>
+                    <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                        <form className="card-body">
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Username</span>
+                                </label>
+                                <input
+                                    type="username"
+                                    placeholder="Enter your Username"
+                                    className="input input-bordered"
+                                    required
+                                    onChange={handleUsernameChange}
+                                    value={username}
+                                />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Password</span>
+                                </label>
+                                <input
+                                    type="password"
+                                    placeholder="Enter your password"
+                                    className="input input-bordered"
+                                    required
+                                    onChange={handlePasswordChange}
+                                    value={password}
+                                />
+                                <label className="label">
+                                    <a className="label-text-alt link link-hover">
+                                        Forgot password?
+                                    </a>
+                                </label>
+                            </div>
+                            <div className="form-control mt-6">
+                                <button onClick={handleLogin} className="btn btn-primary">Login</button>
+                            </div>
+                        </form>
                     </div>
                 </div>
-            </>
-        );
-    }
+            </div>
+        </>
+    )
 }
+
+export default Login
