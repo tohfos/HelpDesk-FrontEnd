f44c50391dd0ceeaeaabca507afa8bc5e2530e8f
Akmas14 <abdullah_kmas@outlook.com>
2023-12-22 17:32:49 +0200

    generate report done w comps analytics created

--- src/pages/Dashboard/Reports/Index.jsx
+++ src/pages/Dashboard/Reports/Index.jsx
@@ -3,11 +3,16 @@ import { toast, ToastContainer } from 'react-toastify'
 import { useEffect, useState } from 'react'
 import { jwtDecode } from 'jwt-decode'
 import Cookies from 'js-cookie'
-
+//import Header from '../../../components/Report/Header'
+import Header from '../../../components/Report/Header'
+import { Routes } from 'react-router-dom'
+import { Route } from 'react-router-dom'
+import GenReport from '../../../components/Report/GenReport'
 
 const Index = () => {
     const [reports, setReports] = useState([])
-
+    const [showGenReport, setShowGenReport] = useState(false)
+    const [newReport,setNewReport] = useState({})
 
     useEffect(() => {
         fetchReports();
@@ -30,7 +35,26 @@ const Index = () => {
                 credentials: 'include'
             })
             const data = await response.json()
-            setReports(data)
+            setNewReport(data)
+        }
+        catch (error) {
+            console.log(error)
+        }
+    }
+    const generateReport = async (id)=>{
+        try {
+            console.log(id.ticketId)
+            const response = await fetch(`${process.env.REACT_APP_EXPRESS_URL}/api/v1/manager/generateReport/${id.ticketId}`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Bearer ' + Cookies.get('token')
+                },
+                credentials: 'include'
+            })
+            const data = await response.json()
+            console.log(data)
+            setNewReport(data)
         }
         catch (error) {
             console.log(error)
@@ -38,7 +62,17 @@ const Index = () => {
     }
 
     return (
-        <div>Reports</div>
+        <div><Header
+        onAdd={() => setShowGenReport(!showGenReport)}
+      />
+      
+              <>
+                {showGenReport && <GenReport onAdd={generateReport} report={newReport.ReportDetails} />}
+                <h3>{newReport.ReportDetails}</h3>
+              </>
+
+    
+           </div>
     )
 }
 export default Index
