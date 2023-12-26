e91f43c8b12126dcb3155806c065c78432cbe5ca
Mahmouddd21 <mahmoudosama11.msed@gmail.com>
2023-12-27 01:13:14 +0200

    bazabt el 404

--- src/pages/404/Index.jsx
+++ src/pages/404/Index.jsx
@@ -7,7 +7,7 @@ const NotFound = () => {
                 <p class="text-6xl md:text-7xl lg:text-9xl font-bold tracking-wider text-accent">404</p>
                 <p class="text-2xl md:text-3xl lg:text-5xl font-bold tracking-wider text-secondary mt-4">Page Not Found</p>
                 <p class="text-primary mt-4 pb-4 border-b-2 text-center">Sorry, the page you are looking for could not be found.</p>
-                <a href="/dashboard" class="flex items-center space-x-2  btn btn-primary px-4 py-2 mt-6 rounded transition duration-150" title="Return Home">
+                <a href="/" class="flex items-center space-x-2  btn btn-primary px-4 py-2 mt-6 rounded transition duration-150" title="Return Home">
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
                         <path fill-rule="evenodd" d="M9.707 14.707a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 1.414L7.414 9H15a1 1 0 110 2H7.414l2.293 2.293a1 1 0 010 1.414z" clip-rule="evenodd"></path>
                     </svg>
